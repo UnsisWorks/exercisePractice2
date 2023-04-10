d8b714c2d5c41bb7287e41852b93d736993a9773
MATEBOOK-13\jared <jared0192galicia@gmail.com>
2023-04-09 23:29:18 -0600

    Add more exercise

--- ciclos/Ejercicio3.java
+++ ciclos/Ejercicio3.java
@@ -1,5 +1,22 @@
 package ciclos;
 
-public class Ejercicio3 {
+import java.util.Scanner;
 
+public class Ejercicio3 {
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		System.out.println("Valor n de una serie");
+		
+		System.out.print("Limite: ");
+		int limit = inp.nextInt();
+		float result = 4;
+		
+		for (int i = 0; i <= limit; i+= 2) {
+			result -= (4/i);
+		}
+		
+		System.out.println("El valor es: " + result);
+		
+		inp.close();
+	}
 }
