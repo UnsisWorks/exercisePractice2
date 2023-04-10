ae67552c18f0601637663c1da2e4f739ce15cd52
MATEBOOK-13\jared <jared0192galicia@gmail.com>
2023-04-10 17:25:15 -0600

    Add more exercise

--- ciclos/Ejercicio5.java
+++ ciclos/Ejercicio5.java
@@ -1,5 +1,23 @@
 package ciclos;
 
-public class Ejercicio5 {
+import java.util.Scanner;
 
+public class Ejercicio5 {
+	public static void main(String[] args) {
+
+		Scanner inp = new Scanner(System.in);
+
+		System.out.print("Ingresa un numero: ");
+		int n = inp.nextInt();
+		float result = 0;
+		
+		for (int i = 0; i <= n; i++) {
+			result += Math.PI * (i * i);
+		}
+		
+		System.out.println("El resultado es: " + result);
+		
+		inp.close();
+
+	}
 }
--- ciclos/Ejercicio6.java
+++ ciclos/Ejercicio6.java
@@ -1,5 +1,34 @@
 package ciclos;
 
-public class Ejercicio6 {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Ejercicio6 {
+	public static void main(String[] args) {
+
+			ArrayList<Integer> numeros = new ArrayList<Integer>();
+			Scanner inp = new Scanner(System.in);
+			
+			int primos = 0;
+			
+			do {
+				System.out.print("Ingresa un numero: ");
+				numeros.add(inp.nextInt());
+				
+				for (int i = 2; i < numeros.get(numeros.size() - 1); i++) {
+					if (numeros.get(numeros.size() - 1) % i == 0 || numeros.get(numeros.size() - 1) == 0) {
+						primos--;
+						break;
+					} else {
+					}
+				}
+				primos++;
+				
+			} while (numeros.get(numeros.size() - 1) != 0);
+		
+			System.out.println("Numeros primos totales: " + primos);
+			
+			inp.close();
+		
+	}
 }
